ecbd0ee4abfaf4b8fce1d1908d261d34cb7092c7
TranHoangChung <vipbroken123@gmail.com>
2023-01-04 08:09:20 +0700

    First_commit_THC110

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TranHoangChung110.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
